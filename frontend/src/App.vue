2d461485bdcc1b7be477b408f6a015abcc69e724
limphaibool <limphaibool@gmail.com>
2024-05-26 16:19:12 +0700

    setup pinia and router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+
 </script>
 
 <template>
   <div>
+    hello
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
+    <RouterView />
   </div>
 </template>
