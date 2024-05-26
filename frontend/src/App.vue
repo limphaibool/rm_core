f840fbf1323545a9bc974a67f0af2cb05bafc778
limphaibool <limphaibool@gmail.com>
2024-05-26 16:33:55 +0700

    add tailwind

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,11 +4,12 @@ import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
-  <div>
-    hello
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-    </nav>
-    <RouterView />
+  <div class="min-h-screen flex flex-col h-full">
+    <header class="h-16 bg-cyan-600 text-white flex justify-between items-center p-4">
+      <h1>Quality Ceramic</h1>
+    </header>
+    <main class="flex-grow flex flex-col bg-blue-200 h-full">
+      <RouterView />
+    </main>
   </div>
 </template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,1 +1,2 @@
-<template>Home view</template>
+<template>
+</template>
