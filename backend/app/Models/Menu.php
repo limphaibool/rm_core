6632c0eb00ed677815b01f48fce30db61eb3b239
limphaibool <limphaibool@gmail.com>
2024-05-26 11:21:59 +0700

    make menu adjacency list

--- backend/app/Models/Menu.php
+++ backend/app/Models/Menu.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
 
 class Menu extends Model
 {
-    use HasFactory;
+    use HasFactory, HasRecursiveRelationships;
+    public function getLocalKeyName()
+    {
+        return 'menu_id';
+    }
 }
