b88e1c9e11af27f9e7b1b7ecf73308889eba20b0
limphaibool <limphaibool@gmail.com>
2024-05-27 20:44:33 +0700

    main layout structure

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,29 +1,100 @@
 <script setup lang="ts">
-import Button from 'primevue/button';
-import Sidebar from 'primevue/sidebar';
 import { ref } from 'vue';
-const visible = ref(false);
+import PanelMenu from 'primevue/panelmenu';
 
+const items = ref([
+    {
+        label: 'Files',
+        icon: 'pi pi-file',
+        items: [
+            {
+                label: 'Documents',
+                icon: 'pi pi-file',
+                items: [
+                    {
+                        label: 'Invoices',
+                        icon: 'pi pi-file-pdf',
+                        items: [
+                            {
+                                label: 'Pending',
+                                icon: 'pi pi-stop'
+                            },
+                            {
+                                label: 'Paid',
+                                icon: 'pi pi-check-circle'
+                            }
+                        ]
+                    },
+                    {
+                        label: 'Clients',
+                        icon: 'pi pi-users'
+                    }
+                ]
+            },
+            {
+                label: 'Images',
+                icon: 'pi pi-image',
+                items: [
+                    {
+                        label: 'Logos',
+                        icon: 'pi pi-image'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: 'Cloud',
+        icon: 'pi pi-cloud',
+        items: [
+            {
+                label: 'Upload',
+                icon: 'pi pi-cloud-upload'
+            },
+            {
+                label: 'Download',
+                icon: 'pi pi-cloud-download'
+            },
+            {
+                label: 'Sync',
+                icon: 'pi pi-refresh'
+            }
+        ]
+    },
+    {
+        label: 'Devices',
+        icon: 'pi pi-desktop',
+        items: [
+            {
+                label: 'Phone',
+                icon: 'pi pi-mobile'
+            },
+            {
+                label: 'Desktop',
+                icon: 'pi pi-desktop'
+            },
+            {
+                label: 'Tablet',
+                icon: 'pi pi-tablet'
+            }
+        ]
+    }
+]);
 
 </script>
 <template>
-    <div class="flex flex-col min-h-screen bg-gradient-to-br from-primary-400 to-primary-50">
-        <Sidebar v-model:visible="visible" header="Sidebar">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat.</p>
-        </Sidebar>
-        <header class=" font-bold align-middle flex space-x-4 items-center p-6 ">
-            <button
-                class="border-2 text-secondary-700 border-secondary-400 hover:bg-secondary-800 hover:text-white transition-colors duration-300 px-2 py-1 rounded-md"
-                @click="visible = !visible">
-                <i class="pi pi-bars"></i>
-            </button>
-            <div class="text-secondary-900">
+    <div class="flex flex-col min-h-screen bg-gradient-to-l from-primary-300 to-white  ">
+        <header
+            class="m-3 rounded-md bg-secondary-900 font-bold align-middle flex space-x-4 items-center p-6 shadow-xl">
+            <div class="text-primary-200">
                 QUALITY CERAMIC
             </div>
         </header>
         <div class="flex-grow flex ">
+            <div class="bg-slate-800 text-white p-4 w-60 mb-3 ml-3 rounded-md shadow-md flex flex-col">
+                <RouterLink to="/users" class="p-2 hover:bg-slate-600 rounded">Users</RouterLink>
+                <RouterLink to="/roles" class="p-2 hover:bg-slate-600 rounded">Roles</RouterLink>
+            </div>
             <main class="flex-grow p-4">
                 <RouterView />
             </main>
