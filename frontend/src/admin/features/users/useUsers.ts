4bd2a91b8a165af71c886d10d0342e5270b40a58
limphaibool <limphaibool@gmail.com>
2024-06-08 23:40:42 +0700

    refac: users composable

--- frontend/src/admin/features/users/useUsers.ts
+++ frontend/src/admin/features/users/useUsers.ts
@@ -1,17 +1,14 @@
 import { ref } from "vue";
 import axios from "axios";
-import { UserListResponse } from "../../interfaces/responses/UserListResponse";
 import { User } from "../../../shared/interfaces/User";
 
 export default function useUsers() {
   const users = ref<User[]>([]);
   const getUsers = async () => {
     try {
-      const res = await axios.get<UserListResponse>("/admin/users");
+      const res = await axios.get<BaseResponse<User[]>>("/admin/users");
       users.value = res.data.data;
-      console.log(res.data);
     } catch (e) {
-      console.log(e);
     }
   };
   const getUserById = async () => {};
