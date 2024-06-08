d0728aebc0bb197cfff5661965a0e89354078f00
limphaibool <limphaibool@gmail.com>
2024-06-08 23:41:26 +0700

    refac: login

---
+++ frontend/src/shared/composables/useLogin.ts
@@ -0,0 +1,20 @@
+import { ref } from "vue";
+import { useAuthStore } from "../stores/auth";
+import axios from "axios";
+
+export default function useLogin() {
+  const authStore = useAuthStore();
+  const form = ref({
+    username: "",
+    password: "",
+  });
+  const login = async () => {
+    await authStore.getCsrfToken();
+    await axios.post("/auth/login", {
+      username: form.value.username,
+      password: form.value.password,
+    });
+    await authStore.getUser();
+  };
+  return { form, login };
+}
