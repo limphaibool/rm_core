4557f2fb774287e92ce60faa11cac582f360e65c
limphaibool <limphaibool@gmail.com>
2024-06-09 23:50:44 +0700

    update controller

--- backend/app/Http/Controllers/AuthUserController.php
+++ backend/app/Http/Controllers/AuthUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Data\UserData;
+use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -16,12 +17,11 @@ class AuthUserController extends Controller
         return $this->success(data: UserData::from(Auth::user()));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(Request $request, User $user)
     {
-        //
+        $user->update($request->all());
+        return $this->success(message: 'User update success', data: UserData::from($user));
+
     }
 
 }
