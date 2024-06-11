6192b3e7556e5dda461b475f28a8f471e10aba78
limphaibool <limphaibool@gmail.com>
2024-06-11 23:01:00 +0700

    #14 update api finish

--- backend/app/Data/UserData.php
+++ backend/app/Data/UserData.php
@@ -3,7 +3,11 @@
 namespace App\Data;
 
 use Exception;
+use Spatie\LaravelData\Attributes\MapName;
+use Spatie\LaravelData\Attributes\MapOutputName;
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Mappers\CamelCaseMapper;
+use Spatie\LaravelData\Mappers\SnakeCaseMapper;
 use Spatie\LaravelData\Optional;
 use Illuminate\Contracts\Validation\Validator;
 use Spatie\LaravelData\Attributes\MapInputName;
@@ -11,20 +15,20 @@ use Spatie\LaravelData\Attributes\Validation\Required;
 
 class UserData extends Data
 {
-    #[MapInputName(SnakeCaseMapper::class)]
     public function __construct(
-        #[MapInputName('user_id'), Required]
+        #[MapName('user_id'), Required]
         public int|Optional $id,
         #[Required]
         public string|Optional $username,
         #[Required]
-        public string|Optional $nameThai,
+        public string|Optional $name_thai,
         #[Required]
-        public string|Optional $nameEng,
+        public string|Optional $name_eng,
         #[Required]
         public string|Optional $email,
         #[Required]
         public bool|Optional $enabled
     ) {
     }
+
 }
