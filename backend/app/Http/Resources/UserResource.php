aacc6bcbc1d9cfb8c2f0376e2727d183a7c1b01c
limphaibool <limphaibool@gmail.com>
2024-06-13 12:45:28 +0700

    #14 refac: base response

--- backend/app/Http/Resources/UserResource.php
+++ backend/app/Http/Resources/UserResource.php
@@ -6,22 +6,6 @@ use App\Data\UserData;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UserResource extends JsonResource
+class UserResource extends BaseResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(Request $request): array
-    {
-        return [
-            'id' => $this->id,
-            'username' => $this->username,
-            'nameThai' => $this->name_thai,
-            'nameEng' => $this->name_eng,
-            'email' => $this->email,
-            'enabled' => $this->enabled,
-        ];
-    }
 }
