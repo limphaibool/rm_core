fe71212c44c6b268c956406705fab03b1f471150
limphaibool <limphaibool@gmail.com>
2024-06-13 12:45:28 +0700

    #14 refac: base response

--- backend/app/Http/Resources/PermissionResource.php
+++ backend/app/Http/Resources/PermissionResource.php
@@ -5,18 +5,6 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class PermissionResource extends JsonResource
+class PermissionResource extends BaseResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(Request $request): array
-    {
-        return [
-            'resource' => $this->permResource->name,
-            'action' => $this->action->code
-        ];
-    }
 }
