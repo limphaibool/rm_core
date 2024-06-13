a8d43bb6ec13d279bfd79a8bc879fafb42f02657
limphaibool <limphaibool@gmail.com>
2024-06-13 15:42:30 +0700

    #14 test auth user

--- backend/tests/Feature/AuthenticationTest.php
+++ backend/tests/Feature/AuthenticationTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Data\UserData;
 use App\Enums\ResponseStatus;
+use App\Models\Role;
 use App\Traits\HttpResponses;
 use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
@@ -43,7 +44,7 @@ class AuthenticationTest extends TestCase
         // Assert
         $response->assertOk();
         $response->assertJsonPath('status', ResponseStatus::SUCCESS);
-        $response->assertJsonPath('data.id', $user->user_id);
+        $response->assertJsonPath('data.userId', $user->user_id);
         $this->assertAuthenticatedAs($user);
         $response->assertCookie('laravel_session');
         $response->assertCookie('XSRF-TOKEN');
@@ -99,7 +100,6 @@ class AuthenticationTest extends TestCase
 
     public function test_logout_user_call_authenticated_api_returns_unauthenticated()
     {
-
         // Arrange
         $user = User::create([
             'user_id' => 1,
@@ -111,6 +111,11 @@ class AuthenticationTest extends TestCase
             'email' => 'thiti@thiti.com',
             'enabled' => true,
         ]);
+
+        $role = Role::create([
+            'role_id' => 1,
+            'role_name' => 'test role'
+        ]);
         $this->actingAs($user);
         $this->assertAuthenticatedAs($user);
 
@@ -119,8 +124,98 @@ class AuthenticationTest extends TestCase
         $response = $this->get('/api/auth/user');
 
         // Assert
-        $this->withExceptionHandling();
         $response->assertUnauthorized();
         $response->assertJsonPath('status', ResponseStatus::UNAUTHENTICATED);
     }
+
+    public function test_auth_user_show_user_success()
+    {
+        $role = Role::create([
+            'role_id' => 1,
+            'role_name' => 'test role'
+        ]);
+        $user = User::create([
+            'user_id' => 1,
+            'name' => 'Thiti Lim',
+            'username' => 'thiti',
+            'password' => '1234',
+            'name_thai' => 'ธิติ',
+            'name_eng' => 'Thiti',
+            'email' => 'thiti@thiti.com',
+            'enabled' => true,
+            'role_id' => $role->role_id
+        ]);
+
+        $response = $this->actingAs($user)->get('/api/auth/user');
+        $response->assertOk();
+        $response->assertJsonPath('data.userId', $user->user_id);
+        $response->assertJsonPath('data.username', $user->username);
+        $response->assertJsonPath('data.role.roleId', $role->role_id);
+    }
+
+    public function test_auth_user_update_user_success()
+    {
+        $role = Role::create([
+            'role_id' => 1,
+            'role_name' => 'test role'
+        ]);
+        $user = User::create([
+            'user_id' => 1,
+            'name' => 'Thiti Lim',
+            'username' => 'thiti',
+            'password' => '1234',
+            'name_thai' => 'ธิติ',
+            'name_eng' => 'Thiti',
+            'email' => 'thiti@thiti.com',
+            'enabled' => true,
+            'role_id' => $role->role_id
+        ]);
+
+        $response = $this->actingAs($user)->patch('/api/auth/user', [
+            'nameThai' => 'เทส',
+            'nameEng' => 'test',
+        ]);
+        $response->assertOk();
+        $this->assertDatabaseHas('users', [
+            'user_id' => 1,
+            'name_thai' => 'เทส',
+            'name_eng' => 'test',
+        ]);
+
+        $response->assertJsonPath('data.userId', $user->user_id);
+        $response->assertJsonPath('data.nameThai', 'เทส');
+        $response->assertJsonPath('data.nameEng', 'test');
+        $response->assertJsonPath('data.role.roleId', $role->role_id);
+    }
+
+    public function test_auth_user_update_user_validate()
+    {
+        $role = Role::create([
+            'role_id' => 1,
+            'role_name' => 'test role'
+        ]);
+        $user = User::create([
+            'user_id' => 1,
+            'name' => 'Thiti Lim',
+            'username' => 'thiti',
+            'password' => '1234',
+            'name_thai' => 'ธิติ',
+            'name_eng' => 'Thiti',
+            'email' => 'thiti@thiti.com',
+            'enabled' => true,
+            'role_id' => $role->role_id
+        ]);
+
+        $response = $this->actingAs($user)->patch('/api/auth/user', [
+            'nameThai' => '',
+            'nameEng' => 'test',
+        ]);
+        $response->assertBadRequest();
+        $response->assertJsonPath('status', ResponseStatus::FORM_INVALID);
+        $response->assertJsonIsArray('data');
+    }
+
+
+
+
 }
