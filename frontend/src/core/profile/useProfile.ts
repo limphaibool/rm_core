e45fc88806dfb83621909deb8e74df6501112f58
limphaibool <limphaibool@gmail.com>
2024-06-13 22:25:29 +0700

    #15 user validation

--- frontend/src/core/profile/useProfile.ts
+++ frontend/src/core/profile/useProfile.ts
@@ -1,4 +1,4 @@
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { User } from "../../shared/interfaces/User";
 import axios from "axios";
 import { useToast } from "primevue/usetoast";
@@ -24,10 +24,20 @@ export default function useProfile() {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
   };
 
+  const isValidName = (name: string): boolean => {
+    return name.trim().length > 0;
+  };
+
   const validate = () => {
     errors.value.email = isValidEmail(user.value.email)
       ? null
       : "Invalid email format";
+    errors.value.nameEng = isValidName(user.value.nameEng)
+      ? null
+      : "Invalid name";
+    errors.value.nameThai = isValidName(user.value.nameThai)
+      ? null
+      : "Invalid name";
   };
 
   const getUser = async () => {
@@ -44,19 +54,23 @@ export default function useProfile() {
       });
     }
   };
+  const isValid = computed(() => {
+    return errors.value.email === null;
+  });
 
   const updateUser = async () => {
     validate();
+    if (!isValid.value) return;
     try {
       const res = await axios.patch<DataResponse<User>>("/auth/user", {
-        nameThai: user.value.nameThai,
-        nameEng: user.value.nameEng,
-        email: user.value.email,
+        nameThai: user.value.nameThai.trim(),
+        nameEng: user.value.nameEng.trim(),
+        email: user.value.email.trim(),
       });
       user.value = res.data.data;
       toast.add({
         severity: "success",
-        summary: "Error",
+        summary: "Success",
         detail: res.data.message,
         life: 3000,
       });
