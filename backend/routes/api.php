4811f488a3deefd8874751e059287e4c5906ddba
limphaibool <limphaibool@gmail.com>
2024-06-18 13:23:26 +0700

    refac: remove unimplemented methods

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,33 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
-    Route::get('menus', [AuthMenuController::class, 'index']);
-    Route::get('permissions', [AuthPermissionController::class, 'index']);
 });
+
 Route::prefix('auth')->middleware('auth:sanctum')->group(function () {
-    Route::get('menus', [AuthMenuController::class, 'index']);
-    Route::get('permissions', [AuthPermissionController::class, 'index']);
     Route::get('user', [AuthUserController::class, 'show']);
-    Route::patch('user', [AuthUserController::class, 'update']);
+    Route::put('user', [AuthUserController::class, 'update']);
 });
 
 Route::prefix('admin')->middleware('auth:sanctum')->group(function () {
     Route::resource('/users', UserController::class);
-    // Route::get('/users', [UserController::class, 'index']);
-    // Route::post('/users', [UserController::class, 'store']);
-    // Route::get('/users/{user_id}', [UserController::class, 'show']);
-    // Route::delete('/users/{user_id}', [UserController::class, 'destroy']);
-
     Route::resource('/roles', RoleController::class);
-    Route::get('/roles/{role_id}', [RoleController::class, 'show']);
-
-    Route::get('roles/{role_id}/permissions', [RolePermissionController::class, 'index']);
-    Route::post('roles/{role_id}/permissions', [RolePermissionController::class, 'store']);
-    Route::delete('roles/{role_id}/permissions', [RolePermissionController::class, 'destroy']);
-
-    Route::get('roles/{role_id}/users', [RolePermissionController::class, 'index']);
-    Route::post('roles/{role_id}/users', [RolePermissionController::class, 'store']);
-
 });
 
 
